a9fc004ce1feb6f452749504bbd56945215292c6
Ubaydullo Asatullaev <Ubaydullohazard@gmail.com>
2023-08-11 16:24:45 +0900

    Create Uninstall.sh

---
+++ snpe/2.12.0.230626/Uninstall/Uninstall.sh
@@ -0,0 +1,5 @@
+qik uninstall "/opt/qcom/aistack/snpe/2.12.0.230626/Uninstall/qualcomm_neural_processing_sdk.2.12.0.230626.Linux-AnyCPU.qik" -silent -config /var/tmp/qcom/qik/cache/qualcomm_neural_processing_sdk.2.12.0.230626.Linux-AnyCPU_installConfig.json
+rm -f "/opt/qcom/aistack/snpe/2.12.0.230626/Uninstall/qualcomm_neural_processing_sdk.2.12.0.230626.Linux-AnyCPU.qik"
+rm -f "/var/tmp/qcom/qik/cache/qualcomm_neural_processing_sdk.2.12.0.230626.Linux-AnyCPU_installConfig.json"
+rm -f "/opt/qcom/aistack/snpe/2.12.0.230626/Uninstall/Uninstall.sh"
+rm -rf "/opt/qcom/aistack/snpe/2.12.0.230626/Uninstall"
